67bd7e62b1503a9cb27c8c7171a960012a74504c
MahmoudMohamedIsmail <mahmoud.ismail2203@gmail.com>
2020-07-22 03:03:23 +0200

    Add Podfile

---
+++ Podfile
@@ -0,0 +1,8 @@
+# platform :ios, '9.0'
+
+target 'Corona Virus Tracker (COVID-19)' do
+  use_frameworks!
+
+  # Pods for Corona Virus Tracker (COVID-19)
+pod 'iOSDropDown'
+end
---
+++ Pods/Target Support Files/Pods-Corona Virus Tracker (COVID-19)/Pods-Corona Virus Tracker (COVID-19)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Corona_Virus_Tracker__COVID_19_ : NSObject
+@end
+@implementation PodsDummy_Pods_Corona_Virus_Tracker__COVID_19_
+@end
